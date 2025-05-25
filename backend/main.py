e7482445d888c196a4fddac3e26b85a853a07943
M1nDBr34k <151804171+M1nDBr34k@users.noreply.github.com>
2025-05-25 19:29:35 +0800

    Fix import statements for models and database in main.py

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
 
-import models, schemas
-from database import SessionLocal, engine
+from . import models, schemas
+from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
